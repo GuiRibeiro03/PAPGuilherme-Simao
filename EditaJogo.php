01e12e3d27adb9d5aca652d6f0bcf09f8ef4ae4c
Smokee43 <72324455+Smokee43@users.noreply.github.com>
2021-03-02 12:53:49 +0000

    Backoffice

---
+++ EditaJogo.php
@@ -0,0 +1,42 @@
+<?php
+include_once ("includes/body.inc.php");
+top();
+?>
+<form action="confirmaEditaJogo.php" method="post" enctype="multipart/form-data" style="color: #FFFFFF; margin-left: 30px">
+<h2>Editar Jogo</h2>
+<hr>
+    <div><img id="output_image" src="img/jogos/cyberpunk2077.png" style="margin-left: 20px; margin-bottom: 20px"/></div>
+<input type="file" name="jogoImagemURL" accept="image/*" onchange="preview_image(event)">
+  <div style="margin-top: 20px">
+    <label>Titulo:</label>
+    <input type="text" name="jogoNome">
+    <label>Preço: </label>
+    <input type="number" name="jogoPreco">
+    <div style="font-size: 20px;">
+    <div><input type="checkbox" name="Tags">Playstation</div>
+    <div><input type="checkbox" name="Tags">Computador</div>
+    <div><input type="checkbox" name="Tags">CD Projekt Red</div>
+    <div><input type="checkbox" name="Tags">Escandalo</div>
+    <div><input type="checkbox" name="Tags">Uma xota</div>
+  </div>
+    </div>
+
+    <input type="Submit" value="Edita"><br>
+</form>
+
+<script>
+function preview_image(event)
+{
+var reader = new FileReader();
+reader.onload = function()
+{
+var output = document.getElementById('output_image');
+output.src = reader.result;
+}
+reader.readAsDataURL(event.target.files[0]);
+}
+</script>
+
+<?php
+bottom();
+?>
--- jogosBackoffice.php
+++ jogosBackoffice.php
@@ -1,15 +1,59 @@
 <?php
 include_once("includes/body.inc.php");
 top();
+
 ?>
 
-<div class="container">
-    <div class="table-striped">
-    <div style="width:230px; height: 200px; margin-bottom: 30px"><img src="img/jogos/cyberpunk2077.png">Cyberpunk 2077<div>69,90€</div></div>
-    </div>
 
-</div>
+
+<table class="table-striped" style="color: black; background-color: #FFFFFF; font-weight: bold; width: 100%; margin-left: 20px">
+
+<tr>
+<td colspan="3" style="margin-bottom: 10px">
+<a href="AdicionaJogo.php" style="color: black">Adicionar</a>
+</td>
+</tr>
+<tr>
+    <th>Id</th>
+    <th>Nome</th>
+    <th>Imagem</th>
+    <th>Preço</th>
+    <th colspan="2">Opções</th>
+</tr>
+
+<tr style="margin-top: 20px">
+<td>#1</td>
+
+<td>Nome do Jogo</td>
+<td><img src="img/jogos/cyberpunk2077.png" style="width: 220px; height: 210px"></td>
+<td>69,90€</td>
+<td><a href="EditaJogo.php">Editar</a></td>
+<td><a href="#" onclick="confirmaElimina(1)">Remover</a></td>
+
+</tr>
+
+
+</table>
 
 <?php
 bottom();
 ?>
+
+<script>
+    function confirmaElimina(id) {
+        if(confirm('Confirma que deseja eliminar o registo com o ID #'+id+"?"))
+            window.location="../elimina/eliminaCanais.php?id=" + id;
+    }
+
+
+    function preview_image(event)
+    {
+        var reader = new FileReader();
+        reader.onload = function()
+        {
+            var output = document.getElementById('output_image');
+            output.src = reader.result;
+        }
+        reader.readAsDataURL(event.target.files[0]);
+    }
+</script>
