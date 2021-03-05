2118820a9f09ab2a7c17334766b510ae403a62ef
Smokee43 <72324455+Smokee43@users.noreply.github.com>
2021-03-05 11:45:14 +0000

    Guilherme-CRUD JOGOS

--- confirmaNovoJogo.php
+++ confirmaNovoJogo.php
@@ -6,9 +6,10 @@ $jogoSinopse=addslashes($_POST["jogoSinopse"]);
 $jogoTrailer=addslashes($_POST["jogoTrailer"]);
 $jogoImagemURL=$_FILES["jogoImagemURl"]["name"];
 $jogoPreco=intval($_POST["jogoPreco"]);
+$jogoempresaId=intval($_POST["jogoEmpresaId"]);
 
-echo $sql="insert into jogos (jogoNome,jogoSinopse,jogoTrailer,jogoPreco) 
-values(0, '".$jogoNome."','".$jogoSinopse."','".$jogoTrailer."','".$jogoImagemURL."', '".$jogoPreco."')";
+echo $sql="insert into jogos (jogoNome,jogoSinopse,jogoTrailer,jogoPreco,jogoEmpresaId) 
+values(0, '".$jogoNome."','".$jogoSinopse."','".$jogoTrailer."','".$jogoImagemURL."', '".$jogoPreco."', '".$jogoempresaId."')";
 
 mysqli_query($con,$sql);
 header("location: jogosBackoffice.php");
