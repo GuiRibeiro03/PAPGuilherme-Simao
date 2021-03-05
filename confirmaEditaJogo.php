08f2c09cc729cdd1a90d381f4cec987b1614fdcc
Smokee43 <72324455+Smokee43@users.noreply.github.com>
2021-03-05 18:20:11 +0000

    Guilherme-confirmaEditaJogo

--- confirmaEditaJogo.php
+++ confirmaEditaJogo.php
@@ -14,7 +14,7 @@ $novoNome="img/jogos/".$img;
 $sql="UPDATE jogos SET jogoNome='".$jogoNome."', jogoSinopse='".$jogoSinopse."', jogoTrailer='".$jogoTrailer."',jogoPreco='".$jogoPreco."', jogoEmpresaId='".$empresaId."'";
 
 if($img!=''){
-    $sql.=", jogoImageURL='".$img."'";
+    $sql.=", jogoImagemURL='".$img."'";
 }
 
 $sql.="where jogoId=".$id;
