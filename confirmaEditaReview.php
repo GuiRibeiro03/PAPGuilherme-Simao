81ab624b0eb7ac44ccf290c33702e81b3f82e10b
Smokee43 <72324455+Smokee43@users.noreply.github.com>
2021-03-10 11:41:25 +0000

    Guilherme-confirmaEditaReview

--- confirmaEditaReview.php
+++ confirmaEditaReview.php
@@ -2,27 +2,27 @@
 $con = mysqli_connect("localhost", "root", "","pap2021gameon");
 
 $id=intval($_GET["id"]);
-$jogoNome=addslashes($_POST["reviewNome"]);
-$jogoSinopse=addslashes($_POST["reviewSinopse"]);
-$jogoPreco=addslashes($_POST["reviewTitulo"]);
-$empresaId=intval($_POST["reviewImagemURL"]);
-$jogoTrailer=addslashes($_POST["jogoPontosPositivos"]);
-$jogoTrailer=addslashes($_POST["jogoPontosNegativos"]);
+$reviewNome=addslashes($_POST["reviewNome"]);
+$reviewSinopse=addslashes($_POST["reviewSinopse"]);
+$reviewTitulo=addslashes($_POST["reviewTitulo"]);
+$reviewCapa=intval($_POST["reviewImagemURL"]);
+$reviewPP=addslashes($_POST["jogoPontosPositivos"]);
+$reviewPN=addslashes($_POST["jogoPontosNegativos"]);
 $img=$_FILES['jogoImagemURL']["name"];
 $novoNome="img/jogos/".$img;
 
 
-$sql="UPDATE jogos SET jogoNome='".$jogoNome."', jogoSinopse='".$jogoSinopse."', jogoTrailer='".$jogoTrailer."',jogoPreco='".$jogoPreco."', jogoEmpresaId='".$empresaId."'";
+$sql="UPDATE reviews SET reviewNome='".$reviewNome."', reviewSinopse='".$reviewSinopse."', reviewTitulo='".$reviewTitulo."',jogoPontosPositivos='".$reviewPP."', jogoPontosNegativos='".$reviewPN."'";
 
 if($img!=''){
-    $sql.=", jogoImagemURL='".$img."'";
+    $sql.=", reviewImagemURL='".$img."'";
 }
 
 $sql.="where jogoId=".$id;
 
 mysqli_query($con,$sql);
 print_r($sql);
-header("location: jogosBackoffice.php");
+header("location: reviewsBackoffice.php");
 
 
 
