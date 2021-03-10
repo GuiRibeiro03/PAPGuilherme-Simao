d92c45bacbd96f94e4061146b9bdb025890268aa
Smokee43 <72324455+Smokee43@users.noreply.github.com>
2021-03-10 17:52:16 +0000

    IGDB API

--- indexAPI.php
+++ indexAPI.php
@@ -1,37 +1,18 @@
 
-
     <script>
-
-        {
-            "access_token": "epz2ym5b3y3uq6skv9xtdqz97v27pj",
-            "expires_in": 4849659,
-            "token_type": "bearer"
-        }
-
-    function auth(id){
+    function auth(){
         $.ajax({
                 POST: "https://id.twitch.tv/oauth2/token",
                 client_id: "x6vzfmpjecjafohpdh8muj2ladt8xd",
                 client_secret:"yfb4t31wos78svif4834jn5jg19mbp",
-                grant_type: "client_credentials",
-                fields "*";
-
-
-                url: "https://api.igdb.com/v4/games",
-                type: "post",
-                data: {
-                        name:id
-                }
-                success: function (auth){
-                        $('#nome').html(auth);
-                }
+                grant_type: "client_credentials"
 
                 });
             }
 
-        $('document').ready(function (){
-           auth();
-        });
+    $('document').ready(function (){
+        alert("hey")
+    });
 </script>
 
 <body>
