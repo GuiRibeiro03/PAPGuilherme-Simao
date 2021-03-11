560b9594d1d782cf0d53701ed96a9acd93c984a1
Smokee43 <72324455+Smokee43@users.noreply.github.com>
2021-03-11 00:32:17 +0000

    Guilherme-API

--- indexAPI.php
+++ indexAPI.php
@@ -1,18 +1,15 @@
-
+<head><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script></head>
     <script>
-    function auth(){
-        $.ajax({
-                POST: "https://id.twitch.tv/oauth2/token",
-                client_id: "x6vzfmpjecjafohpdh8muj2ladt8xd",
-                client_secret:"yfb4t31wos78svif4834jn5jg19mbp",
-                grant_type: "client_credentials"
+        var settings = {
+            "url": "https://id.twitch.tv/oauth2/token?client_id=x6vzfmpjecjafohpdh8muj2ladt8xd&client_secret=yfb4t31wos78svif4834jn5jg19mbp&grant_type=client_credentials",
+            "method": "POST",
+            "timeout": 0,
+        };
 
-                });
-            }
-
-    $('document').ready(function (){
-        alert("hey")
-    });
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            $('#nome').html(response);
+        });
 </script>
 
 <body>
