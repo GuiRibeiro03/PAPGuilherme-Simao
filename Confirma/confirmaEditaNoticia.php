f035dc18df525d21afd5b743733f09ae580ccad7
Smokee43 <72324455+Smokee43@users.noreply.github.com>
2021-03-17 22:34:39 +0000

    Guilherme-Noticias

---
+++ Confirma/confirmaEditaNoticia.php
@@ -0,0 +1,30 @@
+<?php
+$con = mysqli_connect("localhost", "root", "","pap2021gameon");
+
+$id=intval($_GET["id"]);
+$reviewNome=addslashes($_POST["reviewNome"]);
+$reviewSinopse=addslashes($_POST["reviewSinopse"]);
+$reviewTitulo=addslashes($_POST["reviewTitulo"]);
+$reviewCapa=intval($_POST["reviewImagemURL"]);
+$reviewPP=addslashes($_POST["jogoPontosPositivos"]);
+$reviewPN=addslashes($_POST["jogoPontosNegativos"]);
+$img=$_FILES['jogoImagemURL']["name"];
+$novoNome="../img/jogos/".$img;
+
+
+$sql="UPDATE reviews SET reviewNome='".$reviewNome."', reviewSinopse='".$reviewSinopse."', reviewTitulo='".$reviewTitulo."',jogoPontosPositivos='".$reviewPP."', jogoPontosNegativos='".$reviewPN."'";
+
+if($img!=''){
+    $sql.=", reviewImagemURL='".$img."'";
+}
+
+$sql.="where jogoId=".$id;
+
+mysqli_query($con,$sql);
+print_r($sql);
+header("location: ../backoffice/reviewsBackoffice.php");
+
+
+
+
+
---
+++ Edita/EditaNoticia.php
@@ -0,0 +1,79 @@
+<?php
+include_once("../includes/body.inc.php");
+top();
+?>
+
+<div style="height: 60px; width: 100%; background-color: red;"><span style="padding-left: 40%; font-size: 30px; color: #fff; text-shadow: 1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000;">Edita Review</span></div>
+
+<section class="store" style="margin-top: 100px; margin-left: 50px">
+    <a href="../reviews.php"><button type="button" class="btn btn-danger">Voltar</button></a>
+
+
+
+
+
+    <form action="../Edita/ConfirmaEditaReview.php" method="post" enctype="multipart/form-data" style="color: #FFFFFF; font-size: 18px; width: 100%">
+
+
+        <div id="wrapper" style="color: #FFFFFF">
+            <input type="file" accept="image/*" name="reviewImagemURL" onchange="preview_image(event)">
+            <div style="height: 50px">
+            <img id="output_image"/></div>
+        </div>
+        <br>
+
+
+        <div class="mb-3">
+            <label  class="form-label">Titulo</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Titulo" name="reviewNome" style="width: 40%" autofocus>
+        </div>
+
+
+        <div class="mb-3">
+            <label  class="form-label">Review</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="20" name="reviewSinopse" style="width: 70%"></textarea>
+        </div>
+
+
+
+        <div class="mb-3" style="width: 300px">
+            <label  class="form-label">Pontos Positivos:</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1"  rows="5" name="reviewPontosPositivos" ></textarea>
+            <label  class="form-label">Pontos Negativos:</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1"  rows="5" ame="reviewPontosNegativos"  ></textarea>
+
+        </div>
+
+        </div>
+
+
+        <br>
+
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" >
+            <label class="form-check-label" >
+                Review
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" >
+            <label class="form-check-label" >
+                Jogo
+            </label>
+
+        </div>
+        <input type="Submit" value="Adicionar" style="width: 100px; height: 50px">
+
+
+
+    </form>
+</section>
+
+
+
+<?php
+bottom();
+?>
+
+
+
