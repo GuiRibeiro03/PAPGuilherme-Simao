91988987850df214b6815a7357f7e5262527953d
Smokee43 <72324455+Smokee43@users.noreply.github.com>
2021-03-25 17:36:43 +0000

    Guilherme-Confirma Edita Produto

---
+++ Confirma/confirmaEditaProduto.php
@@ -0,0 +1,22 @@
+<?php
+$con = mysqli_connect("localhost", "root", "","pap2021gameon");
+
+$id=intval($_GET["id"]);
+$prodNome=addslashes($_POST["produtoNome"]);
+$prodPreco=addslashes($_POST["produtoPreco"]);
+$prodDesc=addslashes($_POST["produtoDescricao"]);
+$prodImagem=$_FILES["produtoImagemURL"]["name"];
+$prodTipo=addslashes($_POST["produtoTipo"]);
+$novoNome="../img/produtos/".$prodImagem;
+
+
+$sql="update produtos set produtoNome='".$prodNome."',produtoDescricao='".$prodDesc."'";
+
+if(!$prodImagem=''){
+    $sql.=", produtoImagemURL='".$novoNome."'";
+    copy($_FILES['produtoImagemURL']['tmp_name'],$novoNome);
+}
+
+$sql.=", produtoPreco='".$prodPreco."',produtoTipo='".$prodTipo."' where produtoId=".$id;
+mysqli_query($con,$sql);
+header("location: ../backoffice/produtoBackoffice.php");
