52f84bb548f1f8c0e3675b50996d94242327e875
SimaoBercial <simaobercial80@gmail.com>
2021-04-07 20:44:15 +0100

    confirmaEditaTagsJogo mais ou menos funcional

---
+++ Confirma/confirmaEditaTagsJogo.php
@@ -0,0 +1,25 @@
+<?php
+$con = mysqli_connect("localhost", "root", "","pap2021gameon");
+
+$id=intval($_GET["id"]);
+$jogoGeneroGeneroId=addslashes($_POST["generoId"]);
+$jogoPlataformaPlataformaId=addslashes($_POST["plataformaId"]);
+
+
+
+$sql="UPDATE jogogeneros SET jogoGeneroGeneroId='".$jogoGeneroGeneroId."', jogoGeneroJogoId='".$id."'";
+$sql2="UPDATE jogoplataformas SET jogoPlataformaPlataformaId='".$jogoPlataformaPlataformaId."', jogoPlataformaJogoId='".$id."'";
+
+
+mysqli_query($con,$sql);
+mysqli_query($con,$sql2);
+
+print_r($sql);
+print_r($sql2);
+
+header("location: ../backoffice/jogosBackoffice.php");
+
+
+
+
+
