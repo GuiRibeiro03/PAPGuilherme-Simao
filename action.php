ba21429cfc9d887505a3c9875b11d02a41a0a86c
pipe1910 <pipevital@gmail.com>
2021-05-18 16:46:16 +0100

    Guilherme- Filtragem por tags ajax

--- action.php
+++ action.php
@@ -25,7 +25,27 @@ where jogoEmpresaId != '' ";
 
     if($result=num_row>0){
         while ($row=$result=mysqli_fetch_array($result)){
+    $output.= "<div id='content'  class='col-lg-4 col-md-3' style=' margin-bottom: 5%'>
 
+                <div  class='card' style='max-width: 23rem; min-width: 15rem; width: auto; padding-left: 10px; padding-right: 10px; padding-top: 10px; background-color: black'>
+
+                    <a href="Listajogo.php?id=''.$dados['jogoId'].'' "><img src="img/<?php echo $dados["jogoImagemURL"] ?>'"' class="card-img-top" alt="..." style="height: 400px"></a>
+                    <a href="\Listajogo.php?id="" \"></a>
+
+
+                    <div class="card-body">
+
+                        <a href="Listajogo.php?id=<?php echo $dados["jogoId"] ?>"><h5 class="card-title"><?php echo $dados["jogoNome"] ?></h5></a>
+
+                        <p class="card-text" style="font-size: 18px"><strong><?php echo $dados["jogoPreco"] ?>€</strong>&nbsp;&nbsp;<span class="badge bg-success"><i class="fa fa-check"></i></span></p>
+
+                        <a onclick="adicionaCarrinho(<?php echo $dados["jogoId"] ?>)"  style="color: #dc3545;">
+                            <input type="submit" class="cart-button" value="Adicionar ao Carrinho" style="height: 50px; font-weight: bold"></a>
+                    </div>
+
+                </div>
+
+            </div>";
         }
     }
 
