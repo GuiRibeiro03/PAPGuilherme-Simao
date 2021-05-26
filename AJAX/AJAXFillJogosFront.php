3de39d868f41a4909629dff9201558f89da0d25b
pipe1910 <pipevital@gmail.com>
2021-05-26 15:22:24 +0100

    Guilherme- Filtragem por preço,nome e mais recente

--- AJAX/AJAXFillJogosFront.php
+++ AJAX/AJAXFillJogosFront.php
@@ -2,10 +2,24 @@
 include_once("../includes/body.inc.php");
 $txt=addslashes($_POST['txt']);
 $con=mysqli_connect("localhost","root","","pap2021gameon");
-$sql="select * from jogos where jogoNome like '%$txt%' order by jogoId asc  ";
+$sql="select * from jogos where jogoNome like '%$txt%' ";
 $result=mysqli_query($con, $sql);
 
+$ord = 0;
+if (isset($_GET['ord'])) {
+    $ord = $_GET['ord'];
+    if ($ord == 1) {
+        $sql .= " order by jogoNome ASC";
+    } elseif ($ord == 2)  {
+        $sql .= " order by jogoId DESC";
+    } elseif ($ord == 3)  {
+        $sql .= " order by jogoPreco ASC";
+    } elseif ($ord == 4)  {
+        $sql .= " order by jogoPreco DESC";
+    }
 
+}
+$result=mysqli_query($con,$sql);
 ?>
 
 
@@ -90,6 +104,24 @@ $result=mysqli_query($con, $sql);
             </div>
 
     </div>
+
+
+
+    <button onclick="myFunction()" class="dropbtn" style="background-color: #FFF;color: #0d0d0d; outline: solid 2px white; padding: 2px 5px; ">Ordenar produto por:</button>
+    <div id="myDropdown" class="dropdown-content">
+        <a href="jogos.php?ord=1" class="dropdown-item">A-Z</a>
+        <a href="jogos.php?ord=2" class="dropdown-item">Mais recentes</a>
+        <a href="jogos.php?ord=3" class="dropdown-item">Preço Ascendente</a>
+        <a href="jogos.php?ord=4" class="dropdown-item">Preço Decrescente</a>
+    </div>
+
+    </div>
+
+
+
+    <div class="row" style="margin-top: 2%">
+
+
 
 
     <div class="row" >
