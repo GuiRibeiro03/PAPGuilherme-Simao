daa9008ba61d0153f2ea1e506d390282dd192880
Smokee43 <72324455+Smokee43@users.noreply.github.com>
2021-06-06 01:01:52 +0100

    Guilherme - checkout

--- checkout2.php
+++ checkout2.php
@@ -1,42 +1,39 @@
 <?php
 include_once("includes/bodyBase.inc.php");
 top();
+
+$sql="select * from perfis where perfilId=".$_SESSION['id'];
+$res=mysqli_query($con,$sql);
+
+$dados=mysqli_fetch_array($res);
+
 ?>
-<head>
 
 
-
-<script>
-
-    $(document).ready(function() {
-
-        $('.method').on('click', function() {
-            $('.method').removeClass('blue-border');
-            $(this).addClass('blue-border');
-        });
-
-    })
-
-    var $cardInput = $('.input-fields input');
-
-    $('.next-btn').on('click', function(e) {
-
-        $cardInput.removeClass('warning');
-
-        $cardInput.each(function() {
-            var $this = $(this);
-            if (!$this.val()) {
-                $this.addClass('warning');
-            }
-        })
-    });
-</script>
-
-</head>
-   <body style=" background-color: #0d0d0d">
 
      <section class="store" style="margin-top: 20px; margin-bottom: 20px; color: #FFFFFF;background-color: #0d0d0d; margin-left: 10%; margin-right: 10%">
-        <div><h3 style="font-weight: bold">Checkout</h3></div>
+        <div ><h3 style="font-weight: bold">Checkout: Envio</h3></div>
+<br>
+
+         <div style="border-radius: 3px; border: solid 2px white; width: 400px; height:230px; padding: 10px 10px; font-weight: bold; font-size: 20px ">
+        <div>
+            <p ><?php echo $dados['perfilNome']?></p>
+            <p><?php echo $dados['perfilMorada']?></p>
+            <p><?php echo $dados['perfilTelefone']?></p>
+        </div>
+
+             <div style="float: right; padding-bottom: 10px"><input type="radio" style="width: 20px; height: 20px;"></div>
+
+         </div>
+
+
+
+         <div style="border-radius: 3px; border: solid 2px white; width: 500px; height:60px; padding: 15px 10px; font-weight: bold; font-size: 20px; margin-top: 5%">
+             <p>CTT- Expresso: <span>3,90€</span></p><div style="float: right; padding-bottom: 10px"><input type="radio" style="width: 20px; height: 20px;"></div>
+
+         </div>
+
+
 
 
 
@@ -49,7 +46,7 @@ top();
 
 
 
-   </body>
+
 
 
     <!-- Footer Section Begin -->
