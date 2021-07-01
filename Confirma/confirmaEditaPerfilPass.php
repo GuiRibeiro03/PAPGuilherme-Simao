ddc51c1d137ed41ba48a1c94e00d0bd87a2429f8
pipe1910 <pipevital@gmail.com>
2021-07-01 11:52:09 +0100

    Guilherme-Pass login md5

--- Confirma/confirmaEditaPerfilPass.php
+++ Confirma/confirmaEditaPerfilPass.php
@@ -1,26 +1,30 @@
 <?php
 include_once("../includes/body.inc.php");
-$con = mysqli_connect("localhost", "root", "","pap2021gameon");
+$con = mysqli_connect("localhost", "root", "", "pap2021gameon");
 
 
+$id = intval($_GET['id']);
+$sql2 = "select * from users where userId=" . $id;
+$res = mysqli_query($con, $sql2);
+$dados = mysqli_fetch_array($res);
 
-$id=intval($_GET['id']);
-$sql2="select * from users where userId=".$id;
-$res=mysqli_query($con,$sql2);
-$dados=mysqli_fetch_array($res);
+
+$oldPass = addslashes($_POST['oldPass']);
+$newPass = addslashes($_POST['newPass']);
+$newPassEncrypt = md5($newPass);
 
 
-$oldPass=addslashes($_POST['oldPass']);
-$newPass=addslashes($_POST['newPass']);
-$newPassEncrypt=md5($newPass);
-
-if($oldPass===$dados['userPassword'] AND $newPass != $dados['userPassword'] ){
-    $sql="update users set userPassword='".$newPassEncrypt."' where userId=".$id;
+if ($oldPass != $newPassEncrypt) {
+    $sql = "update users set userPassword='" . $newPassEncrypt . "' where userId=" . $id;
+}else{
+    echo "<script>   
+            alert('A palavra-pass nova é igual à antiga!');
+            window.location='../editaPerfil.php?id='.$id;
+        </script>";
 }
-
 
 
 
 print_r($sql);
 mysqli_query($con, $sql);
-header("location: ../perfilUser.php?id=".$id);
+header("location: ../perfilUser.php?id=" . $id);
