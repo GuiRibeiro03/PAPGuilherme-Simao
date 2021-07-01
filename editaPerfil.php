1ca0e3f5a27fb77217b059fc9cb6ffa7258a412a
pipe1910 <pipevital@gmail.com>
2021-07-01 09:59:12 +0100

    Guilherme-Update Password

--- editaPerfil.php
+++ editaPerfil.php
@@ -1,8 +1,9 @@
 <?php
 include_once("includes/bodyBase.inc.php");
 top();
+$con=mysqli_connect(HOST,USER,PASSWORD,DATABASE);
 $id=intval($_SESSION['id']);
-$sql="select * from perfis where perfilUserId=$id";
+$sql="select * from perfis where perfilUserId=".$id;
 $result=mysqli_query($con,$sql);
 $dados=mysqli_fetch_array($result);
 ?>
@@ -12,7 +13,7 @@ $dados=mysqli_fetch_array($result);
 
 <div id="titulo" style="text-align: center; width: 100%; font-weight: bold; margin-top: 2%"><h2>Edita Perfil</h2></div>
     <hr>
-    <form action="Confirma/confirmaEditaPerfil.php?id=<?php echo $dados['perfilId'] ?>" style="color: #FFF; background-color: #1a1a1a; margin-left: 20%; margin-top: 5%;margin-bottom: 5%; width: 50%" method="post" enctype="multipart/form-data">
+    <form action="Confirma/confirmaEditaPerfil.php?id=<?php echo $dados['perfilId'] ?>" style="color: #FFF; margin-left: 20%; margin-top: 5%;margin-bottom: 5%; width: 50%" method="post" enctype="multipart/form-data">
         <div class="mb-3">
             <label for="exampleInputEmail1" class="badge badge-primary" style="font-size: 20px">Nome de Perfil: </label>
             <input type="text" class="form-control" name="perfilNome" value="<?php echo $dados['perfilNome'] ?>">
@@ -39,8 +40,33 @@ $dados=mysqli_fetch_array($result);
         <div class="mb-3">
             <label for="exampleInputEmail1" class="badge badge-primary" style="font-size: 20px">Numero de Telemóvel:</label>
             <input type="text" class="form-control" maxlength="9" style="width: 50%;" placeholder="+351..." name="perfilTele" value="<?php echo $dados['perfilTelefone'] ?>">
-
         </div>
+        <button type="submit" class="btn btn-success">Editar Perfil</button>
+    </form>
+
+
+
+
+    <?php
+    $sql2="select * from users where userId=".$id;
+    $res=mysqli_query($con,$sql2);
+    $dados2=mysqli_fetch_array($res);
+
+    ?>
+    <form action="Confirma/confirmaEditaPerfilPass.php?id=<?php echo $dados2['userId'] ?>" style="color: #FFF; margin-left: 20%; margin-top: 5%;margin-bottom: 5%; width: 50%" method="post" enctype="multipart/form-data">
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="badge badge-primary" style="font-size: 20px">Palavra-Passe:</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" name="oldPass" required>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="badge badge-primary" style="font-size: 20px">Nova Palavra-Passe:</label>
+            <input type="password" class="form-control" maxlength="9" style="width: 50%;"  name="newPass" required>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="badge badge-primary" style="font-size: 20px">Repita a nova Palavra-Passe:</label>
+            <input type="password" class="form-control" maxlength="9" style="width: 50%;"  name="newPass" required>
+        </div>
+
         <button type="submit" class="btn btn-success">Editar Perfil</button>
     </form>
 
