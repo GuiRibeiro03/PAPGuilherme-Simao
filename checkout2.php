886ca6e0718c4cc82438d7368460af2f11f3b86f
Smokee43 <72324455+Smokee43@users.noreply.github.com>
2021-07-01 23:23:49 +0100

    Guilherme

--- checkout2.php
+++ checkout2.php
@@ -1,8 +1,9 @@
 <?php
 include_once("includes/bodyBase.inc.php");
 top();
+$con = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
 
-$sql = "select * from perfis where perfilId=" . $_SESSION['id'];
+$sql = "select * from moradas inner join perfis on perfilId=moradaPerfilId where perfilId=" . $_SESSION['id'];
 $res = mysqli_query($con, $sql);
 $dados = mysqli_fetch_array($res);
 
@@ -12,9 +13,9 @@ $res2 = mysqli_query($con, $sql2);
 $dados2 = mysqli_fetch_array($res2);
 
 
+
+$ctt = '';
 ?>
-
-
 
 
     <div style="width: 100%; text-align: center;  margin-top: 2%">
@@ -22,122 +23,190 @@ $dados2 = mysqli_fetch_array($res2);
     </div>
 
 
-    <section class="store" id="storeStyleCheckout" style="">
+    <section class="store" id="storeStyleCheckout" style="color: #0d0d0d ">
         <br>
 
-        <div class="row-cols-lg-6">
-        <!-- **************************DIV MORADA**************START*********  -->
+        <div class="row">
 
-        <h4 style="font-weight: bold; color: #FFFFFF">Morada de Entrega:</h4>
-        <div id="morada">
-            <div>
-                <p style="color: black"><?php echo $dados['perfilNome'] ?></p>
-                <p style="color: black"><?php echo $dados['perfilMorada'] ?></p>
-                <p style="color: black"><?php echo $dados['perfilTelefone'] ?></p>
-            </div>
-
-            <div style="float: right; padding-bottom: 5px"><input type="radio" style="width: 20px; height: 20px;" checked>  </div>
+            <!-- **************************DIV MORADA**************START*********  -->
+            <?php
+            $sql = "select * from moradas inner join perfis on perfilId=moradaPerfilId where perfilId=" . $_SESSION['id'];
+            $res = mysqli_query($con, $sql);
+            while ($dados = mysqli_fetch_array($res)) {
+                ?>
+                <div id="morada">
+                    <h4 style="font-weight: bold; color: #FFFFFF">Morada de Entrega:</h4>
+                    <div>
+                        <p style="color: black"><?php echo $dados['perfilNome'] ?></p>
+                        <p style="color: black"><?php echo $dados['moradaTexto'] ?></p>
+                        <p style="color: black"><?php echo $dados['moradaTelefone'] ?></p>
+                    </div>
+                    <div style="float: right; padding-bottom: 5px"><input type="radio"
+                                                                          style="width: 20px; height: 20px;" checked>
+                    </div>
+                </div>
+                <?php
+            }
+            ?>
+            <!-- **************************DIV MORADA***************END********  -->
 
         </div>
-        <!-- **************************DIV MORADA***************END********  -->
 
-            <div class="row-cols-lg-7">
-                <div style="width: 400px; height:90px; margin-top: 3%">
-                    <span style="padding: 15px 10px; font-weight: bold; font-size: 35px;">Metodo de Envio:</span>
-                </div>
+        <div class="row" style="margin-top: 5%;">
+            <div id="ctt">
 
-                <div id="ctt">
+                <h4 style="font-weight: bold; color: #FFFFFF">Metodo de Envio:</h4>
+                <div style="margin-top: 20px">
                     <span>CTT- Expresso: 3,90€</span>
-                    <div style="float: right;"><input type="radio" style="width: 20px; height: 20px;" checked></div>
-
+                    <div style="float: right;"><input type="radio" name="jogoDestaque" id="<?php $ctt ?>" value="sim" <?php if ($ctt == 'sim') { echo "checked";} ?>></div>
                 </div>
+
+                <div style="margin-top: 20px">
+                 <span>Recolher em Loja:</span>
+                    <div style="float: right;"><input type="radio" name="jogoDestaque" value="nao"  ></div>
+
+                        <p style="margin-top: 10px"><select>
+                            <option value="Leiria">Leiria</option>
+                            <option value="Leiria">Lisboa</option>
+                            <option value="Leiria">Coimbra</option>
+                            <option value="Leiria">Aveiro</option>
+                            <option value="Leiria">Setúbal</option>
+                            <option value="Leiria">Faro</option>
+                            <option value="Leiria">Porto</option>
+                        </select></p>
+                </div>
+
+
             </div>
-
         </div>
+
+
         <!-- **************************DIV DETALHES***************START********  -->
-        <div class="row-cols-lg-4">
-        <div id="detalhesPedido" style="height: auto; width: 450px; margin-left: 60%; position:relative;">
-            <div style="width: 100%; text-align: center"><span style="font-weight: bold; ">Detalhes do pedido<i class="fa fa-arrow-down"></i></span></div>
+
+        <div id="detalhesPedido" style=" float:right; position: relative;">
+
+            <div style="width: 100%; margin-bottom: 20px; text-align: center"><span style="font-weight: bold; ">Detalhes do pedido &nbsp;<i
+                            class="fa fa-arrow-down"></i></span></div>
             <?php
-            $lista="(0";
-            if(isset($_SESSION['carrinho'])){
-                foreach ($_SESSION['carrinho'] as $produto){    
-                    $lista.=",".$produto;
+            $lista = "(0";
+            if (isset($_SESSION['carrinho'])) {
+                foreach ($_SESSION['carrinho'] as $produto) {
+                    $lista .= "," . $produto;
                 }
             }
-            $lista.=")";
+            $lista .= ")";
 
-            $sql1="select * from produtos where produtoId in ".$lista;
+            $sql1 = "select * from produtos where produtoId in " . $lista;
 
 
+            $result1 = mysqli_query($con, $sql1);
+            $precoTotal = 0;
+            $k = 0;
 
-            $result1=mysqli_query($con,$sql1);
-            $precoTotal=0;
-            $k=0;
-            while($dados2=mysqli_fetch_array($result1)){
+            while ($dados2 = mysqli_fetch_array($result1)) {
 
                 ?>
                 <div style="margin-right: 20px; margin-left: 20px">
-                    <?php  $k++; ?> <img src="img/<?php echo $dados2["produtoImagemURL"] ?>" style="height: 60px; width: 70px;" > <?php echo $dados2["produtoNome"] ?>:</a>
-                    <span id="preco" style="color: #0b0b0b; font-size: 20px"><strong><?php echo $dados2["produtoPreco"] ?>€</strong> </span>
+                    <?php $k++; ?> <img src="img/<?php echo $dados2["produtoImagemURL"] ?>" style="width: 20%;"> <span
+                            style="font-weight: bold"><?php echo $dados2["produtoNome"] ?></span>:</a>
+                    <span id="preco"
+                          style="color: #0b0b0b; font-size: 20px"><strong><?php echo $dados2["produtoPreco"] ?>€</strong> </span>
+                    <?php
+                    if($ctt=='checked') {
+                        $precoTotal+=3.90;
+?>
+                        <span style="color: #0b0b0b; font-size: 20px">Envio: 3,90€</span>
+
+                            <?php
+                    }
+                    ?>
+
+                </div>
 
 
+                <?php
+                $precoTotal += $dados2["produtoPreco"];
+            } ?>
+
+
+
+            <?php
+            $lista = "(0";
+            if (isset($_SESSION['carrinho'])) {
+                foreach ($_SESSION['carrinho'] as $jogo) {
+                    $lista .= "," . $jogo;
+                }
+            }
+            $lista .= ")";
+
+            $sql2 = "select * from jogos where jogoId in $lista";
+
+
+            $result2 = mysqli_query($con, $sql2);
+
+            while ($dados3 = mysqli_fetch_array($result2)) {
+
+                ?>
+                <div style="margin-right: 20px; margin-left: 20px; margin-bottom: 5%; margin-top: 5%">
+                    <?php $k++; ?> <img src="img/<?php echo $dados3["jogoImagemURL"] ?>" style=" width: 20%; "> <span
+                            style="font-weight: bold; margin-left: 20px"> <?php echo $dados3["jogoNome"] ?></span>:
+                    &nbsp;</a>
+                    <span id="preco"
+                          style="color: #0b0b0b; font-size: 20px"><strong><?php echo $dados3["jogoPreco"] ?>€</strong> </span>
+                    <?php
+                    if($ctt=='checked') {
+                        $precoTotal+=3.90;
+                        ?>
+                        <span style="color: #0b0b0b; font-size: 20px">Envio: 3,90€</span>
+
+                        <?php
+                    }
+                    ?>
                 </div>
 
 
 
                 <?php
-                $precoTotal+=$dados2["produtoPreco"];
-            }?>
+
+                $precoTotal += $dados3["jogoPreco"];
 
 
 
-            <?php
-            $lista="(0";
-            if(isset($_SESSION['carrinho'])){
-                foreach ($_SESSION['carrinho'] as $jogo){
-                    $lista.=",".$jogo;
-                }
+
             }
-            $lista.=")";
-
-            $sql2="select * from jogos where jogoId in $lista";
 
 
-            $result2=mysqli_query($con,$sql2);
 
-            while($dados3=mysqli_fetch_array($result2)){
+            ?>
+            <div style="width: 100%;"><span
+                        style="font-weight: bold; color: #0d0d0d; font-size: 20px; margin-left: 20px">Total: <?php echo $precoTotal ?>€</span>
+                <a>
+                    <button type="button" class="btn btn-danger" style="float: right; background-color: red">
+                        Encomendar
+                    </button>
+                </a></div>
 
-                ?>
-                <div style="margin-right: 20px; margin-left: 20px">
-                    <?php  $k++; ?> <img src="img/<?php echo $dados3["jogoImagemURL"] ?>" style=" width: 20%; outline: solid 1px black;" > <?php echo $dados3["jogoNome"] ?>: &nbsp;</a>
-                    <span id="preco" style="color: #0b0b0b; font-size: 20px"><strong><?php echo $dados3["jogoPreco"] ?>€</strong> </span>
-
-
-                </div>
-                <?php
-
-                $precoTotal+=$dados3["jogoPreco"];
-            }?>
         </div>
+
+
         <!-- **************************DIV DETALHES***************END********  -->
         </div>
 
 
-
-        <div class="row" >
+        <div class="row">
             <div class="col-75">
                 <span style="padding: 15px 10px; font-weight: bold; font-size: 35px;">Método de Pagamento:</span>
                 <div class="container">
 
                     <form action="/action_page.php">
 
-                        <div class="row" style="outline: solid 3px gray; background-color: #FFFFFF; width: 70%; padding: 5px 20px 15px 20px; color: #0d0d0d ">
+                        <div class="row"
+                             style="outline: solid 3px gray; background-color: #FFFFFF; width: 70%; padding: 5px 20px 15px 20px; color: #0d0d0d ">
 
                             <div class="col-50">
 
                                 <label for="fname">Accepted Cards</label>
-                                <div class="icon-container" >
+                                <div class="icon-container">
                                     <i class="fa fa-cc-visa" style="color:navy;"></i>
                                     <i class="fa fa-cc-amex" style="color:blue;"></i>
                                     <i class="fa fa-cc-mastercard" style="color:red;"></i>
@@ -163,7 +232,9 @@ $dados2 = mysqli_fetch_array($res2);
                                 </div>
                             </div>
 
-                            <input type="submit" style="background-color: red; color:#FFFFFF; margin-top: 5%; height: auto; padding: 5px 10px; float: right" value="Guardar">
+                            <input type="submit"
+                                   style="background-color: red; color:#FFFFFF; margin-top: 5%; height: auto; padding: 5px 10px; float: right"
+                                   value="Guardar">
                         </div>
 
 
@@ -172,7 +243,6 @@ $dados2 = mysqli_fetch_array($res2);
             </div>
 
         </div>
-
 
 
     </section>
