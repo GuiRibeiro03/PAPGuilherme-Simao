3b5cbc9d34e1ae85bb940e28b439814cf546ed98
pipe1910 <pipevital@gmail.com>
2021-07-02 13:34:47 +0100

    Guilherme-Carrinho Elimina

--- AJAX/AJAXRemoverProdutosDoCarrinho.php
+++ AJAX/AJAXRemoverProdutosDoCarrinho.php
@@ -3,8 +3,11 @@ include_once ("../includes/bodyBase.inc.php");
 $id=intval($_GET['id']);
 $array=$_SESSION['carrinho'];
 
+
+
+
 foreach (array_keys($array, $id) as $key) {
-    unset($array[$key]);
+    unset($_SESSION['carrinho'][$key]);
 }
 
 header("location: ".$_SERVER['HTTP_REFERER']);
